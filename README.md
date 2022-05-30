300101ad0be3ed9fd0299bdbc9f374ac40d8fe27
Dariusz Gawlik <97292765+DaroGawlik@users.noreply.github.com>
2022-05-30 20:24:10 +0200

    feat: add new buttons to remove cards, add cards feat: add initial login view feat: add express endpoints to service logging, removing and adding accounts. feat: create README.md

---
+++ README.md
@@ -0,0 +1,45 @@
+# WebApplication for all plants freaks, who wants got all knowledge about your colletion in one place. 
+
+
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+
+Hello, 
+I am working on application that users can add new bought plants. 
+Appliction will save all information about flower like:
+- photo from first and successive days while grow up for check progress
+- need air humidity in the house 
+- when last water and option for make reminder
+- and some smaller information
+
+### Now
+At this moment you can see main page with registration users: 
+- add account by name and password also choose image from disc to your profile
+- delete one and all accounts by button
+
+### Next
+In next step:
+- offer default five avatars to profile 
+- activate "login" button to enter account with resources
+- build responisve for higher resolution 
+
+## Technologies
+- JavaScript
+- Css
+- Sass
+- Gulp
+
+also apply method "Mobile First". Default resolution is 320 x 568.
+
+## Setup
+First of all clone repository by command
+
+```
+$ git clone git@github.com:DaroGawlik/Project.git
+```
+To run this project, I'm using VSC's plugin LiveServer.
+
--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,15 @@
+// const imagemin = require('gulp-imagemin')
+
 const section = document.querySelector('section')
 
+const btnLogin = document.querySelector('.btn-login')
 const addNewAccountBtn = document.querySelector('.btn-add')
 const deleteAccountBtn = document.querySelector('.btn-delete')
 const deleteAllAccountBtn = document.querySelector('.btn-all-delete')
 
 const newAccountPanel = document.querySelector('.add-new-account')
-const saveBtn = document.querySelector('.save')
-const cancelBtn = document.querySelector('.cancel')
+const saveNewAccountPanelBtn = document.querySelector('.save')
+const cancelNewAccountPanelBtn = document.querySelector('.cancel')
 
 const alertAddAccount = document.querySelector('.alert-add-account')
 const pass1text = document.querySelector('.pass1-text')
@@ -16,31 +19,53 @@ const nameInput = document.querySelector('#username')
 const pass1Input = document.querySelector('#password1')
 const pass2Input = document.querySelector('#password2')
 
+const loginViewShadow = document.querySelector('.login-view-shadow')
+const loginViewPanel = document.querySelector('.login-view')
+const pass3InputLoginPanel = document.querySelector('#password3')
+const openLoginPanelBtn = document.querySelector('.open-login')
+const cancelLoginPanelBtn = document.querySelector('.cancel-login')
+const alertLoginView = document.querySelector('.alert-login-view')
+
 const avatar = document.querySelector('#avatar')
+const forwardAvatarBtn = document.querySelector('.forward-avatar')
+const deleteForwardAvatarBtn = document.querySelector('.delete-forward-avatar')
+const avatarDefault = document.querySelectorAll('.avatarDefault')
+const defaultsAvatars = document.querySelector('.defaults-avatars')
 const reader = new FileReader()
 
 const mainView = document.querySelector('.main-view')
 const alertInfo = document.querySelectorAll('.alert-info')
 const activeAcc = document.querySelector('.active-acc')
 
+let card
 let oneCard
 let fotoFromDisc
+let temporaryPhoto
+let isZoom = false
+let oneTargetDefaultAvatar
 let ID = 0
-let accountsArr = []
+const accountsArr = []
 
 /// SHOW PANEL TO CRETAE NEW ACCOUNT
 
 const showPanel = () => {
 	newAccountPanel.style.display = 'flex'
 	mainView.style.display = 'none'
+	if (avatar && avatar.getAttribute('fotoFromDisc')) {
+		avatar.removeAttribute('fotoFromDisc')
+	}
+
+	showBtnsInAddAccountPanel()
 }
 
 /// CLEAR ALL INPUTS IN PANEL TO ADD NEW ACCOUNT
 
 const clearValuePanel = e => {
-	;[username, pass1Input, pass2Input].forEach(el => {
+	;[nameInput, pass1Input, pass2Input, avatar].forEach(el => {
 		el.value = ''
 	})
+	oneTargetDefaultAvatar = undefined
+	avatarDefault.forEach(item => item.classList.remove('avatarDefaultBig'))
 }
 
 /// CLOSE PANEL TO ADD NEW ACCOUNT
@@ -49,19 +74,29 @@ const closePanel = () => {
 	;[alertAddAccount, pass1text, pass2text].forEach(el => {
 		el.innerText = ''
 	})
-	clearValuePanel()
+	// clearValuePanel()
 	newAccountPanel.style.display = 'none'
 	mainView.style.display = 'block'
+	setIsZoom(false)
+}
+
+const showBtnsInMianView = () => {
+	card = document.getElementsByClassName('account-card')
+	;[btnLogin, deleteAccountBtn, deleteAllAccountBtn].forEach(el => {
+		el.style.display = card.length > 0 ? 'block' : 'none'
+	})
+	;[deleteAccountBtn, btnLogin].forEach(el => {
+		el.setAttribute('disabled', true)
+	})
 }
 
 /// CHECK NAME
 
 const checkName = () => {
-	const card = document.getElementsByClassName('account-card')
-
-	if (nameInput.value == '') {
+	const existingCards = document.getElementsByClassName('account-card')
+	if (nameInput.value === '') {
 		alertAddAccount.innerText = 'Put your name'
-	} else if (card.length > 3) {
+	} else if (existingCards.length > 3) {
 		alertAddAccount.innerText = 'You can have max 4 account'
 	} else {
 		alertAddAccount.innerText = ''
@@ -71,7 +106,7 @@ const checkName = () => {
 /// CHECK PASSWORD LENGTH
 
 const checkPasswordLength = () => {
-	if (pass1Input.value == '') {
+	if (pass1Input.value === '') {
 		pass1text.innerText = 'Put your password'
 	} else if (pass1Input.value.length < 1) {
 		pass1text.innerText = 'Your password need 7 letter'
@@ -83,7 +118,7 @@ const checkPasswordLength = () => {
 /// CHECK PASSWORD
 
 const checkPassword = () => {
-	if (pass2Input.value == '') {
+	if (pass2Input.value === '') {
 		pass2text.innerText = 'Put your repeat password'
 	} else if (pass1Input.value !== pass2Input.value) {
 		pass2text.innerText = 'Passwords dont match'
@@ -94,81 +129,355 @@ const checkPassword = () => {
 
 /// ADD NEW ACCOUNT
 
+const createNewCard = (cardID, nickName) => {
+	// console.log(typeof cardID, typeof nickName)
+	const newAccount = document.querySelector('section')
+	const accountCard = document.createElement('div')
+	const opacityBg = document.createElement('div')
+
+	const circle = document.createElement('div')
+	const letter = document.createElement('div')
+
+	const square = document.createElement('div')
+	const name = document.createElement('div')
+
+	newAccount.appendChild(accountCard)
+	accountCard.classList.add('account-card')
+	accountCard.setAttribute('id', cardID)
+	chooseAvatar(opacityBg, accountCard)
+
+	accountCard.appendChild(circle)
+	circle.classList.add('circle')
+	circle.appendChild(letter)
+	letter.classList.add('letter')
+	letter.innerHTML = `${nickName.charAt(0).toUpperCase()}`
+
+	accountCard.appendChild(square)
+	square.classList.add('square')
+	square.appendChild(name)
+	name.classList.add('name')
+	name.innerHTML = `${nickName}`
+	// console.log(cardID, nickName)
+}
+
 const createNewAccount = () => {
-	if (alertAddAccount.textContent == '' && pass1text.textContent == '' && pass2text.textContent == '') {
-		const newAccount = document.querySelector('section')
-		const accountCard = document.createElement('div')
-		accountCard.setAttribute('id', ID)
-		accountCard.classList.add('account-card')
-		newAccount.appendChild(accountCard)
+	if (alertAddAccount.textContent === '' && pass1text.textContent === '' && pass2text.textContent === '') {
+		const payload = {
+			name: `${nameInput.value}`,
+			password: `${pass2Input.value}`,
+			backgroundImage: '&{}',
+			id: ID,
+		}
 
-		accountCard.innerHTML = `
-	        <div class="circle">
-	            <div class="letter">${nameInput.value.charAt(0).toUpperCase()}</div>
-	        </div>
-	        <div class="square">
-	            <div class="name">${nameInput.value}</div>
-	        </div>`
-		accountCard.style.backgroundImage = `url(${avatar.getAttribute('fotoFromDisc')})`
-		alertInfo.forEach(item => (item.style.display = 'none'))
+		avatar.removeAttribute('fotoFromDisc')
+		alertInfo.forEach(item => {
+			item.style.display = 'none'
+		})
 		activeAcc.style.display = 'block'
-
-		accountsArr.push(ID)
-
+		// accountsArr.push(ID)
+		createUsers(payload)
 		closePanel()
-		ID++
+		ID += 1
 	}
 }
 
 /// TARGET CARD
 
-const checkAccount = e => {
+const getOneCard = e => {
 	oneCard = e.target.closest('.account-card')
+	if (oneCard !== undefined) {
+		;[deleteAccountBtn, btnLogin].forEach(el => {
+			el.removeAttribute('disabled')
+		})
+	}
 }
 
 /// DELETE BY TARGET CARD
 
 const deleteOneAccount = () => {
+	card = document.getElementsByClassName('account-card')
+	if (oneCard === undefined) {
+		showAlertDeleteByOnceCard()
+		return
+	}
+	if (oneCard === undefined) {
+		return
+	}
+
 	oneCard.remove()
-	const card = document.getElementsByClassName('account-card')
-	if (card.length == 0) {
-		alertInfo.forEach(item => (item.style.display = 'block'))
+
+	if (card.length === 0) {
+		alertInfo.forEach(item => {
+			item.style.display = 'block'
+		})
 		activeAcc.style.display = 'none'
 	}
+	deleteUser((oneCard.hasAttribute('id') && oneCard.getAttribute('id')) || oneCard.id)
+	oneCard = undefined
+}
+
+const showAlertDeleteByOnceCard = () => {
+	alert("YOU HAVEN'T CHOSEN ACCOUNT TO DELETE ")
 }
 
 /// DELETE ALL ACCOUNT
 
 const deleteAllAccount = () => {
 	document.querySelectorAll('.account-card').forEach(item => item.remove())
-	alertInfo.forEach(item => (item.style.display = 'block'))
+	alertInfo.forEach(item => {
+		item.style.display = 'block'
+	})
 	activeAcc.style.display = 'none'
 }
 
-///
-
-/// ADD AVATAR TO NEW ACCOUNT PANEL CARD
+/// ADD AVATAR BY USER
 
 document.querySelector('#newAccountForm').addEventListener('submit', e => {
-	reader.readAsDataURL(avatar.files[0])
 	e.preventDefault()
+	if (avatar.value !== '') {
+		reader.readAsDataURL(avatar.files[0])
+	}
 })
 
 reader.addEventListener('load', e => {
+	e.preventDefault()
 	avatar.setAttribute('fotoFromDisc', e.target.result)
-	document.querySelector('#newAccountForm').setAttribute('src', e.target.result)
+	// showBtnsInAddAccountPanel()
 })
 
+const deleteForwardAvatar = () => {
+	avatar.removeAttribute('fotoFromDisc')
+	avatar.value = ''
+	showBtnsInAddAccountPanel()
+}
+
+avatar.addEventListener('change', e => {
+	showBtnsInAddAccountPanel()
+})
+// 	// showBtnsInAddAccountPanel()
+// 	console.log(this.value)
+// 	if (e.target.value != '') {
+// 		temporaryPhoto = e.target.value
+// 		console.log(temporaryPhoto)
+// 	}
+// 	console.log(typeof e.target.value)
+// 	if (!e.target.result) {
+// 		e.target.result = 'darek.png'
+// 		console.log(e.target.result)
+// 		return
+// 	}
+// })
+
+const showBtnsInAddAccountPanel = () => {
+	;[deleteForwardAvatarBtn, forwardAvatarBtn].forEach(el => {
+		el.style.display = avatar.value !== '' ? 'block' : 'none'
+	})
+}
+
+//  ZOOM AND TARGET DEFAULT AVATAR
+
+const setIsZoom = booleanValue => {
+	isZoom = booleanValue
+}
+
+const extendPhoto = e => {
+	const ZoomAvatar = document.querySelectorAll('.avatarDefaultBig')
+	const avatarDefaultBigContains = e.target.classList.contains('avatarDefaultBig')
+
+	if (isZoom && !avatarDefaultBigContains) {
+		return
+	}
+	if (ZoomAvatar.length === 0) {
+		e.target.classList.toggle('avatarDefaultBig')
+		setIsZoom(true)
+	}
+	if (ZoomAvatar.length === 1) {
+		ZoomAvatar.forEach(item => item.classList.remove('avatarDefaultBig'))
+		setIsZoom(false)
+	}
+}
+
+const targetDefaultAvatar = e => {
+	oneTargetDefaultAvatar = e.target.closest('.avatarDefault')
+}
+
+// CHOOCE AVATAR BY USER
+
+const chooseAvatar = (opacityBg, accountCard) => {
+	if (avatar.getAttribute('fotoFromDisc')) {
+		opacityBg.style.backgroundImage = `url(${avatar.getAttribute('fotoFromDisc')})`
+		const test = `url(${avatar.getAttribute('fotoFromDisc')})`
+		console.log(test)
+
+		addClassOpacity(opacityBg, accountCard)
+	} else if (oneTargetDefaultAvatar) {
+		addClassOpacity(opacityBg, accountCard)
+
+		const GetComputedStyle = getComputedStyle(oneTargetDefaultAvatar).getPropertyValue('background-image')
+
+		console.log(GetComputedStyle)
+
+		opacityBg.style.backgroundImage = GetComputedStyle
+	}
+}
+
+const addClassOpacity = (opacityBg, accountCard) => {
+	accountCard.appendChild(opacityBg)
+	opacityBg.classList.add('opacity-Bg')
+}
+
+//  LOGIN PANEL
+
+const showLoginPanel = () => {
+	loginViewShadow.style.display = 'flex'
+}
+
+const closeLoginPanel = () => {
+	loginViewShadow.style.display = 'none'
+	mainView.style.display = 'block'
+	alertLoginView.innerText = ''
+	pass3InputLoginPanel.value = ''
+	oneCard = undefined
+}
+
+const openLoginPanel = () => {
+	if (pass3InputLoginPanel.value === '') {
+		alertLoginView.innerText = 'Put your password'
+	} else if (pass3InputLoginPanel.value !== pass1Input.value) {
+		alertLoginView.innerText = 'Passwords dont match'
+	} else {
+		alertLoginView.innerText = ''
+	}
+	pass3InputLoginPanel.value = ''
+	oneCard = undefined
+}
+
+// API
+
+const createUsers = payload => {
+	fetch('http://localhost:4000/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(payload),
+	})
+		.then(res => res.json())
+		// .then((post) => {})
+		.then(() => {
+			fetchUsers()
+		})
+
+		.catch(() => {
+			console.log('wystąpił bład w callToApi')
+		})
+}
+
+const fetchUsers = () => {
+	document.querySelectorAll('.account-card').forEach(item => item.remove())
+	fetch('http://localhost:4000/users', {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
+		.then(res => res.json())
+		.then(download => {
+			if (download) {
+				download.forEach(item => {
+					createNewCard(item.id, item.name)
+				})
+			}
+		})
+		.then(() => {
+			clearValuePanel()
+		})
+		.then(() => {
+			showBtnsInMianView()
+			const fetchUsersAccCard = document.getElementsByClassName('account-card')
+			if (fetchUsersAccCard.length > 0) {
+				alertInfo.forEach(item => {
+					item.style.display = 'none'
+				})
+				activeAcc.style.display = 'block'
+			}
+		})
+		.catch(err => {
+			console.log('wystąpił bład w callFromApi')
+			console.log(err)
+		})
+}
+
+const deleteUser = id => {
+	fetch(`http://localhost:4000/users/${id}`, {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then(res => console.log(res.json()))
+}
+
+const deleteUsers = () => {
+	fetch('http://localhost:4000/users', {
+		method: 'DELETE',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	}).then(res => res.json())
+}
+
+// const displayUsersAccount = fetchUsers => {}
 ///  LISTENER
 
 addNewAccountBtn.addEventListener('click', showPanel)
-saveBtn.addEventListener('click', e => {
+saveNewAccountPanelBtn.addEventListener('click', e => {
+	e.preventDefault()
 	checkName()
 	checkPasswordLength()
 	checkPassword()
 	createNewAccount()
+	setIsZoom(false)
 })
-cancelBtn.addEventListener('click', closePanel)
-section.addEventListener('click', checkAccount)
-deleteAccountBtn.addEventListener('click', deleteOneAccount)
-deleteAllAccountBtn.addEventListener('click', deleteAllAccount)
+cancelNewAccountPanelBtn.addEventListener('click', e => {
+	e.preventDefault()
+	fetchUsers()
+	closePanel()
+})
+section.addEventListener('click', getOneCard)
+
+section.addEventListener('click', e => {
+	getOneCard(e)
+	if (oneCard != undefined) {
+		if (oneCard.firstChild.style.backgroundImage === '') {
+			oneCard.classList.add('cardWithOutBg')
+		} else {
+			oneCard.classList.add('cardWithBg')
+		}
+	}
+})
+deleteAccountBtn.addEventListener('click', () => {
+	deleteOneAccount()
+	showBtnsInMianView()
+})
+deleteAllAccountBtn.addEventListener('click', () => {
+	deleteAllAccount()
+	deleteUsers()
+	showBtnsInMianView()
+})
+deleteForwardAvatarBtn.addEventListener('click', deleteForwardAvatar)
+avatarDefault.forEach(item => item.addEventListener('click', extendPhoto))
+avatarDefault.forEach(item => item.addEventListener('click', targetDefaultAvatar))
+btnLogin.addEventListener('click', showLoginPanel)
+cancelLoginPanelBtn.addEventListener('click', e => {
+	e.preventDefault()
+	closeLoginPanel()
+	showBtnsInMianView()
+})
+openLoginPanelBtn.addEventListener('click', e => {
+	e.preventDefault()
+	openLoginPanel()
+})
+
+window.onload = () => {
+	fetchUsers()
+}
